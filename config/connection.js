2cacdbe4b0fd8058471ce45afde212137c2f960f
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-06-01 20:37:41 -0400

    fixes for maria

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,14 @@ var mysql = require("mysql");
 
 var connection; 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createCibbectuib(process.env.JAWSDB_URL)
+connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 
 
  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
+  port : 3306,
   database: "kahunaburger_db"
 });
 
